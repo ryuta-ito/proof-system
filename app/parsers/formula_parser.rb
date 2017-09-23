75800af8c9c1fac1920a9f4094c4bcaf185d99a9
ryuta-ito <ir47839@gmail.com>
2017-09-24 00:05:59 +0900

    add FormulaParser.divide_most_low_prority_operation

--- app/parsers/formula_parser.rb
+++ app/parsers/formula_parser.rb
@@ -12,10 +12,28 @@ module FormulaParser
       disjunction_divide([ParenthesesParser, OperationParser], data)
     end
 
+    def divide_most_low_priority_operation(data)
+      splited_data = split(ParenthesesParser.strip_edge_parentheses data)
+      return [splited_data.join, '', :atom] if splited_data.size == 1
+
+      op_data = most_low_priority_operation(splited_data)
+      middle_index = splited_data.index(op_data)
+      [ splited_data[0..(middle_index-1)].join, splited_data[(middle_index+1)..(-1)].join, Formula.operation.invert[op_data] ]
+    end
+
     private
 
     def start_state
       Start.new
+    end
+
+    def most_low_priority_operation(splited_data)
+      _, op_data = splited_data.map(&:strip).select do |data|
+        data.match Formula.operation_reg_exp
+      end.map do |op_data|
+        [Formula.priority_by_data(op_data), op_data]
+      end.min
+      op_data
     end
   end
 
--- spec/parsers/formula_parser_spec.rb
+++ spec/parsers/formula_parser_spec.rb
@@ -36,7 +36,8 @@ describe FormulaParser do
         ['A', '=> B ∨ φ(x)', :accepted],
         ['φ(x)', '=> B ∨ φ(x)', :accepted],
         ['=>', 'B ∨ φ(x)', :accepted],
-        ['∧', 'B ∨ φ(x)', :accepted]
+        ['∧', 'B ∨ φ(x)', :accepted],
+        ['φ(x)', '', :accepted]
       ].each do |left_data, right_data, result|
         let(:left_data) { left_data }
         let(:right_data) { right_data }
@@ -54,6 +55,32 @@ describe FormulaParser do
         let(:result) { result }
         it { is_expected.to eq divided_data }
       end
+    end
+  end
+
+  describe '#divide_most_low_priority_operation' do
+    subject { FormulaParser.divide_most_low_priority_operation(data) }
+    let(:divided_data) { [left_data, right_data, operation_code] }
+    let(:data) { "#{left_data}#{operation}#{right_data}" }
+
+    Formula.operation.values.each do |operation|
+      context operation do
+        let(:left_data) { '(A ∧ B)' }
+        let(:right_data) { 'C' }
+        let(:operation) { operation }
+        let(:operation_code) { Formula.operation.invert[operation] }
+
+        it { is_expected.to eq divided_data }
+      end
+    end
+
+    context 'atom' do
+      let(:left_data) { 'φ(x)' }
+      let(:right_data) { '' }
+      let(:operation) { '' }
+      let(:operation_code) { :atom }
+
+      it { is_expected.to eq divided_data }
     end
   end
 end
