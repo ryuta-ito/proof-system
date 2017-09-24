8750cbf2f486c16344c77da569d91ac5946b959f
ryuta-ito <ir47839@gmail.com>
2017-09-24 14:36:31 +0900

    change constant and variable schema

--- app/models/term.rb
+++ app/models/term.rb
@@ -6,9 +6,9 @@ class Term
       case term_data
       when %r{^\w+\(}
         Function.build(term_data)
-      when %r{^[a-z]}
-        Constant.build(term_data)
       when %r{^[A-Z]}
+        Constant.build(term_data)
+      when %r{^[a-z]}
         Variable.build(term_data)
       else
         raise "#{term_data} <- invalid term format"
