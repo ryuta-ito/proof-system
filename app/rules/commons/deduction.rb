cac16fd35193ab4fd5743e9ecbae7e4f63ef69a9
ryuta-ito <ir47839@gmail.com>
2017-09-24 12:56:23 +0900

    fix Rules::Commons::Deduction.upper_proofs_satisfy?

--- app/rules/commons/deduction.rb
+++ app/rules/commons/deduction.rb
@@ -3,9 +3,9 @@ require 'rules/commons'
 module Rules::Commons::Deduction
   def upper_proofs_satisfy?(deductions)
     not_obvious_deductions = deductions.select { |deduction| !deduction.obvious? }
-
     not_obvious_deductions.all? do |deduction|
-      deduction.upper_proofs.all? do |upper_proof|
+      not_obvious_upper_proofs = deduction.upper_proofs.select { |proof| !proof.obvious? }
+      not_obvious_upper_proofs.all? do |upper_proof|
         (deductions - [deduction]).any? do |other_deduction|
           upper_proof.axiom.identify?(other_deduction.lower_proof.axiom) &&
             upper_proof.theorem.identify?(other_deduction.lower_proof.theorem)
