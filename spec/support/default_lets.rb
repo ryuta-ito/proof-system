10849c38d0cac6409863b9efe946269bd7c4cc80
ryuta-ito <ir47839@gmail.com>
2017-09-30 13:08:00 +0900

    fix default lets

--- spec/support/default_lets.rb
+++ spec/support/default_lets.rb
@@ -6,4 +6,20 @@ shared_context 'default lets' do
   %w[A B C].each do |c|
     let("constant_#{c}".to_sym) { Constant.build(c) }
   end
+
+  %w[f].each do |fun|
+    %w[A B x y].each do |arg_1|
+      let("function_#{fun}_#{arg_1}") { Function.build("#{fun}(#{arg_1})") }
+
+      %w[A B x y].each do |arg_2|
+        let("function_#{fun}_#{arg_1}_#{arg_2}") { Function.build("#{fun}(#{arg_1} #{arg_2})") }
+      end
+    end
+  end
+
+  %w[x y].each do |var|
+    %w[A B].each do |c|
+      let("substitution_#{c}_#{var}".to_sym) { Substitutions::Substitution.new(Constant.build(c), Variable.build(var)) }
+    end
+  end
 end
