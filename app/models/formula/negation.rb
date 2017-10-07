b84561296c725030529f3ac83fd0505f0579f461
ryuta-ito <ir47839@gmail.com>
2017-10-07 22:29:40 +0900

    add negation model

---
+++ app/models/formula/negation.rb
@@ -0,0 +1,49 @@
+# negation:
+#   ¬<formula>
+
+class Negation < Formula
+  attr_accessor :str, :formula
+
+  class << self
+    def build(negation_data)
+      new.tap do |negation|
+        negation.formula = Formula.build(parse_negation negation_data)
+      end
+    end
+
+    def code
+      '¬'
+    end
+
+    private
+
+    def parse_negation(negation_data)
+      negation_data.sub(%r{\w*#{code}}, '')
+    end
+  end
+
+  def str
+    "#{self.class.code}#{formula.str}"
+  end
+
+  def identify?(target_formula)
+    self.class === target_formula && target_formula.formula.identify?(formula)
+  end
+
+  def free_variables
+    formula.free_variables
+  end
+
+  def substitute(target, replace)
+    return replace if identify?(target)
+    return self unless self.class === target
+
+    new.tap do |negation|
+      negation.formula = formula.substitute(target, replace)
+    end
+  end
+
+  def unify(target_formula)
+    (self.class === target_formula) ? formula.unify(target_formula) : NonUnifier.build
+  end
+end
