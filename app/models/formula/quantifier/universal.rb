daadaa06f887d8baadfef4b517892b170374f8d4
ryuta-ito <ir47839@gmail.com>
2017-10-31 20:43:43 +0900

    tweak comments

--- app/models/formula/quantifier/universal.rb
+++ app/models/formula/quantifier/universal.rb
@@ -6,13 +6,13 @@ class Universal < Formula::Quantifier
     '∀'
   end
 
-  # delta
+  # gamma
   def deductive_sequents_consequence(sequent)
     instance_formula = formula.substitute(bounded_variable, sequent.non_used_constant)
     [ Sequent.new( assumption: sequent.assumption, consequence: sequent.consequence.substitute(self, instance_formula) ) ]
   end
 
-  # gamma
+  # delta
   def deductive_sequents_assumption(sequent)
     instance_formulas = sequent.least_constants.map { |constant| formula.substitute(bounded_variable, constant) }
     [ Sequent.new( assumption: sequent.assumption.add_formulas(instance_formulas), consequence: sequent.consequence ) ]
