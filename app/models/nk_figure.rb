102e79b4b56dd74c673c787f4ebed9cb7a314e99
ryuta-ito <ir47839@gmail.com>
2017-11-01 19:33:49 +0900

    不必要になったメソッドを削除

--- app/models/nk_figure.rb
+++ app/models/nk_figure.rb
@@ -3,13 +3,10 @@
 #   ...
 #   <deduction_n>.
 
-require 'rules/commons/deduction'
-
 class NkFigure
   attr_accessor :root_deduction
 
   include ActiveModel::Model
-  include Rules::Commons::Deduction
   extend Forwardable
 
   def_delegators :root_deduction, :show, :satisfy?
