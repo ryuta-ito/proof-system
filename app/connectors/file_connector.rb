091aaf8c81f90a8ca877d20a043388c3407cc376
ryuta-ito <ir47839@gmail.com>
2017-11-06 16:50:27 +0900

    何となくコメントアウト機能を追加

--- app/connectors/file_connector.rb
+++ app/connectors/file_connector.rb
@@ -1,7 +1,7 @@
 class FileConnector
   class << self
     def read(path)
-      file_read_raw(path)
+      commentout_filter file_read_raw(path)
     end
 
     private
@@ -13,6 +13,13 @@ class FileConnector
                File.join(ProofSystem.file_base, _path)
              end
       (File.read path).chomp
+    end
+
+    def commentout_filter(data)
+      (data.split("\n")
+       .map { |row| row.sub(/#.*$/, '')})
+       .reject(&:empty?)
+       .join("\n")
     end
   end
 end
